b53eb5a73a1cc2fdb87de25ff1f0bd6d9dbfe58c
Anshuman Kundu <anshuman.kundu1@incedoinc.com>
2023-06-09 12:02:07 +0530

    first commit

---
+++ server/models/Post.js
@@ -0,0 +1,40 @@
+import mongoose, { model } from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+        type:String,
+        required:true
+    },
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    location: String,
+    description: String,
+    userPicturePath: String,
+    picturePath:String,
+    likes:{
+        type:Map,
+        of:Boolean,
+    },
+    comments:{
+        type:Array,
+        default:[]
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
